18be2a25683befe5f64d837fc9836afcb9766288
Thanasan Kumdee <6200313@kvis.ac.th>
2021-08-16 00:05:19 +0700

    d'

--- src/pages/projects/countdown.tsx
+++ src/pages/projects/countdown.tsx
@@ -1,10 +1,6 @@
 import { Container } from "../../components/Container";
 const Tanabata = () => {
-  return (
-    <Container myTitle="Thanasan Kumdee">
-      
-    </Container>
-  );
+  return <Container myTitle="Thanasan Kumdee"></Container>;
 };
 
 export default Tanabata;
