14b18141e7df01d6c36549d825a215e1f407340d
itzmeowww <winthanasan@gmail.com>
2022-03-09 21:47:03 +0700

    minor changes

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,11 +1,6 @@
 import { Flex, useColorMode, FlexProps } from "@chakra-ui/react";
-import Head from "next/head";
 
-interface ContainerProps extends FlexProps {
-  myTitle?: string;
-  myDesc?: string;
-}
-export const Container = (props: ContainerProps) => {
+export const Container = (props: FlexProps) => {
   const { colorMode } = useColorMode();
 
   const bgColor = { light: "gray.50", dark: "gray.900" };
@@ -13,21 +8,6 @@ export const Container = (props: ContainerProps) => {
   const color = { light: "black", dark: "white" };
   return (
     <>
-      <Head>
-        <title>
-          {props.myTitle !== undefined
-            ? props.myTitle
-            : "Thanasan Kumdee | ItzMeOwww"}
-        </title>
-        <meta
-          name="description"
-          content={
-            props.myDesc !== undefined
-              ? "Hi! I'm Thanasan Kumdee, a random student with strong passions in coding, music, and art."
-              : props.myDesc
-          }
-        ></meta>
-      </Head>
       <Flex
         overflowX="hidden"
         maxW="100%"
