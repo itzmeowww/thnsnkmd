c4013885b539067a3e0d53b47f22eb6d289a20bc
itzmeowww <winthanasan@gmail.com>
2022-10-24 13:23:04 +0900

    add timer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    fontFamily: {
-      sans: ["Roboto", "sans-serif"],
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
     },
-    extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
 };
