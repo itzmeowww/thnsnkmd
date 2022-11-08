ace907cfaae2d2a883df3f39c8542ed7d988acee
itzmeowww <winthanasan@gmail.com>
2022-11-08 23:40:04 +0900

    feat: update data

--- src/projects.ts
+++ src/projects.ts
@@ -12,7 +12,7 @@ export type Project = {
 export const projects: Project[] = [
   {
     title: "Looooong Cat",
-    body: "Create a very long cat",
+    body: "Create an extremely long cat",
     links: [
       { href: "https://looooongcat.vercel.app", title: "Looooong Cat" },
       {
@@ -23,15 +23,37 @@ export const projects: Project[] = [
     img: "/images/looooong-cat/icon.jpg",
     preview: [
       "/images/looooong-cat/icon.jpg",
-      "/images/looooong-cat/icon.jpg",
-      "/images/looooong-cat/icon.jpg",
+      "/images/looooong-cat/screenshot_1.jpeg",
+      "/images/looooong-cat/screenshot_2.jpeg",
     ],
     slug: "looooong-cat",
     tools: ["svelte", "tailwind"],
   },
   {
+    title: "Beyond Tic Tac Toe",
+    body: "Even more challenging tic tac toe game, empowered with AI player",
+    links: [
+      {
+        href: "https://beyond-tic-tac-toe.vercel.app",
+        title: "Beyond Tic Tac Toe",
+      },
+      {
+        href: "https://github.com/itzmeowww/beyond-tic-tac-toe",
+        title: "GitHub",
+      },
+    ],
+    img: "/images/beyond-tic-tac-toe/icon.jpeg",
+    preview: [
+      "/images/beyond-tic-tac-toe/screenshot_1.jpeg",
+      "/images/beyond-tic-tac-toe/screenshot_2.jpeg",
+      "/images/beyond-tic-tac-toe/screenshot_3.jpeg",
+    ],
+    slug: "beyond-tic-tac-toe",
+    tools: ["nextjs", "tailwind"],
+  },
+  {
     title: "Geacher",
-    body: "Website for finding tutors from KVIS",
+    body: " Find your tutors from KVIS",
     links: [
       { href: "https://geacher.thnsnkmd.com", title: "Geacher" },
       {
@@ -40,7 +62,12 @@ export const projects: Project[] = [
       },
     ],
     img: "/images/geacher/icon.jpg",
-    preview: ["/images/geacher/icon.jpg", "/images/geacher/icon.jpg"],
+    preview: [
+      "/images/geacher/icon.jpg",
+      "/images/geacher/4.jpg",
+      "/images/geacher/5.jpg",
+      "/images/geacher/6.jpg",
+    ],
     slug: "geacher",
     tools: ["nextjs", "tailwind"],
   },
@@ -60,9 +87,10 @@ export const projects: Project[] = [
     img: "/images/beta-task-toggler/icon.jpg",
     preview: [
       "/images/beta-task-toggler/preview.png",
-      "/images/beta-task-toggler/screenshot_1.jpg",
-      "/images/beta-task-toggler/screenshot_2.jpg",
-      "/images/beta-task-toggler/screenshot_3.jpg",
+      "/images/beta-task-toggler/screenshot_1.png",
+      "/images/beta-task-toggler/screenshot_2.png",
+      "/images/beta-task-toggler/screenshot_3.png",
+      "/images/beta-task-toggler/screenshot_4.png",
     ],
     slug: "beta-task-toggler",
     tools: ["bootstrap", "javascript"],
