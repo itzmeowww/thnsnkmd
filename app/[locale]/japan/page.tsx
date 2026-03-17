c73aef983e1ff4d0524d6f47d55a8192558df1e4
itzmeowww <winthanasan@gmail.com>
2026-03-17 22:49:53 +0900

    feat: add prefecture

--- app/[locale]/japan/page.tsx
+++ app/[locale]/japan/page.tsx
@@ -30,7 +30,8 @@ const has_been: { to: Prefecture, on: Date | undefined }[] = [
     { to: "Akita", on: new Date("19 July 2025") },
     { to: "Iwate", on: new Date("20 July 2025") },
     { to: "Ishikawa", on: new Date("16 Jan 2026") },
-    { to: "Fukui", on: new Date("17 Jan 2026") }
+    { to: "Fukui", on: new Date("17 Jan 2026") },
+    { to: "Oita", on: new Date("14 Mar 2026") }
 
 ]
 export const metadata: Metadata = {
